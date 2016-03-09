dba2e16d97090536ce5393db6a383826c3a1fb8e
Nathan <nathant93@gmail.com>
2016-03-08 22:44:07 -0500

    Fix lein-template version

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,5 +1,5 @@
-{:repl {:plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]
-                  [refactor-nrepl "2.0.0-SNAPSHOT"]]
-        :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
-
-{:user {:plugins [[luminus/lein-template "2.9.9.2"]]}}
+{:user {:plugins      [[cider/cider-nrepl "0.11.0-SNAPSHOT"]
+                       [refactor-nrepl "2.0.0-SNAPSHOT"]
+                       [luminus/lein-template "2.9.9.2"]]
+        :dependencies [[alembic "0.3.2"]
+                       [org.clojure/tools.nrepl "0.2.12"]]}}
