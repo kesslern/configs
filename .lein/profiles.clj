cec1c94682349984cd708bcc8b05895a053a7e92
Nathan Kessler <nathant93@gmail.com>
2016-08-04 15:53:45 -0400

    Removing lein profiles

--- .lein/profiles.clj
+++
@@ -1,5 +0,0 @@
-{:user {:plugins      [[cider/cider-nrepl "0.12.0-SNAPSHOT"]
-                       [refactor-nrepl "2.2.0-SNAPSHOT"]
-                       [luminus/lein-template "2.9.9.2"]]
-        :dependencies [[alembic "0.3.2"]
-                       [org.clojure/tools.nrepl "0.2.12"]]}}
