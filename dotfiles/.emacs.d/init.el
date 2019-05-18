8bf5c3c61d62216312b6bb893e3304c77d11bc92
Nathan Kessler <nathant93@gmail.com>
2019-05-18 19:14:00 -0400

    Remove underline from active mode line

--- dotfiles/.emacs.d/init.el
+++ dotfiles/.emacs.d/init.el
@@ -146,6 +146,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(mode-line ((t (:background "gray18" :foreground "#839496" :box (:line-width 1 :color "#073642" :style unspecified) :overline "#073642" :underline "#284b54"))))
+ '(mode-line ((t (:background "gray18" :foreground "#839496" :box (:line-width 1 :color "#073642" :style unspecified) :overline "#073642" :underline nil))))
  '(org-block-begin-line ((t (:inherit org-meta-line :underline nil))))
- '(telephone-line-accent-active ((t (:inherit mode-line :background "darkslateblue" :foreground "white")))))
+ '(telephone-line-accent-active ((t (:inherit mode-line :background "darkslateblue" :foreground "white" :underline nil)))))
