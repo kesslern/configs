b1c96f341fc5a8b254bb8207386d23da95378359
Nathan Kessler <nathant93@gmail.com>
2020-01-08 10:40:32 -0500

    Preserve aspect ratio when cycling backgrounds

--- dotfiles/.bin/wallpaper-cycle.sh
+++ dotfiles/.bin/wallpaper-cycle.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 while true; do
-    feh --no-fehbg --bg-scale --recursive --randomize "${HOME}/.wallpapers/"
+    feh --no-fehbg --bg-fill --recursive --randomize "${HOME}/.wallpapers/"
     sleep 1200
 done
