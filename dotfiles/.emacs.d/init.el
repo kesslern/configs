ad686b54f6fe1c63b38bf8d5f0f4ec4677d1e275
Nathan Kessler <nathant93@gmail.com>
2020-02-26 16:46:19 -0500

    Better emacs shortcuts

--- dotfiles/.emacs.d/init.el
+++ dotfiles/.emacs.d/init.el
@@ -95,6 +95,8 @@
     '("\\` " "\\*helm" "\\*helm-mode" "\\*Echo Area" "\\*Minibuf"))
   (global-set-key (kbd "M-x") 'helm-M-x)
   (global-set-key (kbd "C-x b") 'helm-mini)
+  (global-set-key (kbd "C-x C-b") 'helm-mini)
+  (global-set-key (kbd "C-x f") 'helm-find-files)
   (global-set-key (kbd "C-x C-f") 'helm-find-files))
 
 (use-package smart-hungry-delete
