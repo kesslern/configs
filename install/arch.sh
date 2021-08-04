8c2e649e4c34f90ba0ef1ecf126848455248a0c0
Nathan Kessler <nathant93@gmail.com>
2021-08-03 20:40:54 -0400

    Detect already installed portions

--- install/arch.sh
+++ install/arch.sh
@@ -2,6 +2,7 @@
 
 echo "Installing dependencies..."
 sudo pacman -S --noconfirm \
+  adobe-source-code-pro-fonts \
   compton \
   emacs \
   exa \
@@ -17,13 +18,33 @@ sudo pacman -S --noconfirm \
   tmux \
   xclip \
   zsh \
+|| exit 1
 
 yay -S --noconfirm \
   dot-templater \
-  
+  ttf-nanum \
+|| exit 1
 
 echo "Installing oh-my-zsh..."
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+if [ -e ~/.oh-my-zsh ]; then
+  echo "oh-my-zsh already installed. Skipping..."
+else
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" \
+  || exit 1
+fi
 
 echo "Installing tmux plugin manager..."
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+if [ -e ~/.tmux/plugins/tpm ]; then
+  echo "tmux plugin manager already installed. Skipping..."
+else
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm \
+  || exit 1
+fi
+
+echo "Installing spacemacs"
+if [ -e ~/.emacs.d/init.el ]; then
+  echo "spacemacs already installed. Skipping..." 
+else
+  git clone --depth 1 https://github.com/syl20bnr/spacemacs ~/.emacs.d \
+  || exit 1
+fi
