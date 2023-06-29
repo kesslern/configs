96e99a5a4ecaa63a25b508243535cf5d8efb3901
Nathan Kessler <nathant93@gmail.com>
2023-06-29 19:25:00 -0400

    Remove unused emacs config

--- dotfiles/.emacs.d/init.el
+++ dotfiles/.emacs.d/init.el
@@ -122,7 +122,6 @@
   (message (current-time-string)))
 
 ;; Custom key sequences.
-(global-set-key (kbd "C-c t") 'show-current-time)
 (global-set-key (kbd "C-c d") 'delete-trailing-whitespace)
 
 ;; Auto-add parens
@@ -133,30 +132,6 @@
 
 ;; Enable mouse in terminal
 (xterm-mouse-mode 1)
-
-;; Smart move-to-begginning-of-line command
-(defun smarter-move-beginning-of-line (arg)
-  "Move point back to indentation of beginning of line.
-Move point to the first non-whitespace character on this line.
-If point is already there, move to the beginning of the line.
-Effectively toggle between the first non-whitespace character and
-the beginning of the line.
-If ARG is not nil or 1, move forward ARG - 1 lines first.  If
-point reaches the beginning or end of the buffer, stop there."
-  (interactive "^p")
-  (setq arg (or arg 1))
-
-  ;; Move lines first
-  (when (/= arg 1)
-    (let ((line-move-visual nil))
-      (forward-line (1- arg))))
-
-  (let ((orig-point (point)))
-    (back-to-indentation)
-    (when (= orig-point (point))
-      (move-beginning-of-line 1))))
-(global-set-key [remap move-beginning-of-line]
-                'smarter-move-beginning-of-line)
 
 ;; Scroll by 5 lines at at time
 (setq scroll-step 5)
