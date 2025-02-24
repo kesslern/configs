45d1da16d7f4fc045b8e6fa4c608708622cbe725
Nathan Kessler <nathant93@gmail.com>
2025-02-23 23:05:08 -0500

    Update emacs config to latest emfy; fix backspace region

--- dotfiles/.emacs.d/init.el
+++ dotfiles/.emacs.d/init.el
@@ -1,7 +1,16 @@
-;;; Based off Emfy 0.3.0-dev <https://github.com/susam/emfy>
+;;; init.el --- Emfy 0.4.0 <https://github.com/susam/emfy>
 
-;; Customize user interface.
-(menu-bar-mode 0)
+;;; Commentary:
+
+;; A very basic dark and sleek Emacs setup for general purpose
+;; editing and programming.
+
+;;; Code:
+
+
+;;; Look and Feel ====================================================
+
+;; Tweak UI.
 (when (display-graphic-p)
   (tool-bar-mode 0)
   (scroll-bar-mode 0))
@@ -10,27 +19,39 @@
 
 ;; Dark theme.
 (load-theme 'wombat)
-(set-face-background 'default "#111")
-(set-face-background 'cursor "#c96")
-(set-face-background 'isearch "#c60")
-(set-face-foreground 'isearch "#eee")
-(set-face-background 'lazy-highlight "#960")
-(set-face-foreground 'lazy-highlight "#ccc")
-(set-face-foreground 'font-lock-comment-face "#fc0")
+(with-eval-after-load 'wombat-theme
+  (set-face-background 'default "#111")
+  (set-face-background 'cursor "#c96")
+  (set-face-foreground 'font-lock-comment-face "#fc0")
+  (set-face-background 'isearch "#ff0")
+  (set-face-foreground 'isearch "#000")
+  (set-face-background 'lazy-highlight "#990")
+  (set-face-foreground 'lazy-highlight "#000"))
 
-;; Interactively do things.
-(ido-mode 1)
-(ido-everywhere)
-(setq ido-enable-flex-matching t)
-(fido-mode)
+;; Enable line numbers while writing config, code, or text.
+(dolist (hook '(prog-mode-hook conf-mode-hook text-mode-hook))
+  (add-hook hook 'display-line-numbers-mode))
 
-;; Show stray whitespace.
-(setq-default show-trailing-whitespace t)
+;; Highlight matching pairs of parentheses.
+(setq show-paren-delay 0)
+(show-paren-mode)
+
+;; Auto-complete inputs in the minibuffer.
+(fido-vertical-mode)
+
+
+;;; Whitespace =======================================================
+
+;; Show trailing whitespace while writing config, code, or text.
+(dolist (hook '(conf-mode-hook prog-mode-hook text-mode-hook))
+  (add-hook hook (lambda () (setq show-trailing-whitespace t))))
+
+;; Show stray blank lines.
 (setq-default indicate-empty-lines t)
 (setq-default indicate-buffer-boundaries 'left)
 
 ;; Add a newline automatically at the end of a file while saving.
-(setq-default require-final-newline t)
+(setq require-final-newline t)
 
 ;; Consider a period followed by a single space to be end of sentence.
 (setq sentence-end-double-space nil)
@@ -46,9 +67,8 @@
 (setq js-indent-level 2)
 (setq css-indent-offset 2)
 
-;; Highlight matching pairs of parentheses.
-(setq show-paren-delay 0)
-(show-paren-mode)
+
+;;; Clean Working Directories ========================================
 
 ;; Write auto-saves and backups to separate directory.
 (make-directory "~/.tmp/emacs/auto-save/" t)
@@ -61,59 +81,12 @@
 ;; Disable lockfiles.
 (setq create-lockfiles nil)
 
-;; Workaround for https://debbugs.gnu.org/34341 in GNU Emacs <= 26.3.
-(when (and (version< emacs-version "26.3") (>= libgnutls-version 30603))
-  (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3"))
-
 ;; Write customizations to a separate file instead of this file.
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (load custom-file t)
 
-;; Enable installation of packages from MELPA.
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
 
-;; Install packages.
-(dolist (package '(company
-                   lsp-mode
-                   markdown-mode
-                   paredit
-                   rainbow-delimiters
-                   rust-mode
-                   smart-hungry-delete
-                   string-inflection))
-  (unless (package-installed-p package)
-    (package-install package)))
-
-;; Enable Paredit.
-(add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
-(add-hook 'eval-expression-minibuffer-setup-hook 'enable-paredit-mode)
-(add-hook 'ielm-mode-hook 'enable-paredit-mode)
-(add-hook 'lisp-interaction-mode-hook 'enable-paredit-mode)
-(add-hook 'lisp-mode-hook 'enable-paredit-mode)
-(add-hook 'rust-mode-hook 'enable-paredit-mode)
-
-;; Enable Rainbow Delimiters.
-(add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
-(add-hook 'ielm-mode-hook 'rainbow-delimiters-mode)
-(add-hook 'lisp-interaction-mode-hook 'rainbow-delimiters-mode)
-(add-hook 'lisp-mode-hook 'rainbow-delimiters-mode)
-(add-hook 'rust-mode-hook 'rainbow-delimiters-mode)
-
-;; Customize Rainbow Delimiters.
-(require 'rainbow-delimiters)
-(set-face-foreground 'rainbow-delimiters-depth-1-face "#c66")  ; red
-(set-face-foreground 'rainbow-delimiters-depth-2-face "#6c6")  ; green
-(set-face-foreground 'rainbow-delimiters-depth-3-face "#69f")  ; blue
-(set-face-foreground 'rainbow-delimiters-depth-4-face "#cc6")  ; yellow
-(set-face-foreground 'rainbow-delimiters-depth-5-face "#6cc")  ; cyan
-(set-face-foreground 'rainbow-delimiters-depth-6-face "#c6c")  ; magenta
-(set-face-foreground 'rainbow-delimiters-depth-7-face "#ccc")  ; light gray
-(set-face-foreground 'rainbow-delimiters-depth-8-face "#999")  ; medium gray
-(set-face-foreground 'rainbow-delimiters-depth-9-face "#666")  ; dark gray
+;;; Custom Command and Key Sequences =================================
 
 ;; Custom command.
 (defun show-current-time ()
@@ -122,41 +95,96 @@
   (message (current-time-string)))
 
 ;; Custom key sequences.
+(global-set-key (kbd "C-c t") 'show-current-time)
 (global-set-key (kbd "C-c d") 'delete-trailing-whitespace)
 
-;; Auto-add parens
-(add-hook 'rust-mode-hook 'electric-pair-mode)
 
-;; Enable LSP
-(add-hook 'rust-mode-hook #'lsp)
+;;; Emacs Server =====================================================
 
-;; Enable mouse in terminal
+(require 'server)
+(unless (server-running-p)
+  (server-start))
+
+
+;;; Package Setup ====================================================
+
+(defun install-packages ()
+  "Install and set up packages for the first time."
+  (interactive)
+  (require 'package)
+  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+  (package-refresh-contents)
+  (dolist (package '(markdown-mode paredit rainbow-delimiters mwim))
+    (unless (package-installed-p package)
+      (package-install package))))
+
+
+;;; Paredit Configuration ============================================
+
+;; Enable Paredit on various Lisp modes.
+(when (fboundp 'paredit-mode)
+  (dolist (hook '(emacs-lisp-mode-hook
+                  eval-expression-minibuffer-setup-hook
+                  ielm-mode-hook
+                  lisp-interaction-mode-hook
+                  lisp-mode-hook))
+    (add-hook hook 'enable-paredit-mode)))
+
+;; Do not bind RET to paredit-RET which prevents input from being
+;; evaluated on RET in M-:, ielm, etc.
+(with-eval-after-load 'paredit
+  (define-key paredit-mode-map (kbd "RET") nil)
+  (define-key backward-delete-char (kbd "BACKSPACE") nil))
+
+
+;;; Rainbow Delimiters Configuration =================================
+
+(when (fboundp 'rainbow-delimiters-mode)
+  (dolist (hook '(emacs-lisp-mode-hook
+                  ielm-mode-hook
+                  lisp-interaction-mode-hook
+                  lisp-mode-hook))
+    (add-hook hook 'rainbow-delimiters-mode)))
+
+(with-eval-after-load 'rainbow-delimiters
+  (set-face-foreground 'rainbow-delimiters-depth-1-face "#c66")  ; red
+  (set-face-foreground 'rainbow-delimiters-depth-2-face "#6c6")  ; green
+  (set-face-foreground 'rainbow-delimiters-depth-3-face "#69f")  ; blue
+  (set-face-foreground 'rainbow-delimiters-depth-4-face "#cc6")  ; yellow
+  (set-face-foreground 'rainbow-delimiters-depth-5-face "#6cc")  ; cyan
+  (set-face-foreground 'rainbow-delimiters-depth-6-face "#c6c")  ; magenta
+  (set-face-foreground 'rainbow-delimiters-depth-7-face "#ccc")  ; light gray
+  (set-face-foreground 'rainbow-delimiters-depth-8-face "#999")  ; medium gray
+  (set-face-foreground 'rainbow-delimiters-depth-9-face "#666")) ; dark gray
+
+;; Enable mouse
 (xterm-mouse-mode 1)
-
-;; Scroll by 5 lines at at time
-(setq scroll-step 5)
-
-;; Auto-insert newlines at end of file
-(setq require-final-newline t)
 
 ;; Scroll to top and bottom
 (setq scroll-error-top-bottom t)
 
+;; Auto-insert newlines at end of file
+(setq require-final-newline t)
+
 ;; Restore place when re-opening a file
 (save-place-mode 1)
 
-;; Enable hungry delete
-(smart-hungry-delete-add-default-hooks)
-(global-set-key (kbd "<backspace>") 'smart-hungry-delete-backward-char)
-(global-set-key (kbd "<delete>") 'smart-hungry-delete-backward-char)
-(global-set-key (kbd "C-d") 'smart-hungry-delete-forward-char)
-(setq backward-delete-char-untabify-method 'all)
+(global-set-key (kbd "C-a") 'mwim-beginning-of-code-or-line)
+(global-set-key (kbd "C-e") 'mwim-end-of-code-or-line)
+(global-set-key (kbd "<home>") 'mwim-beginning-of-line-or-code)
+(global-set-key (kbd "<end>") 'mwim-end-of-line-or-code)
 
 ;; Delete trailing whitespace on save
 (add-hook 'before-save-hook
           'delete-trailing-whitespace)
 
-;; Start server.
-(require 'server)
-(unless (server-running-p)
-  (server-start))
+(add-to-list 'load-path "~/.emacs.d/")
+(require 'rgbds-mode)
+
+(delete-selection-mode 1)
+
+;;; The End ==========================================================
+
+(provide 'init)
+
+;;; init.el ends here
